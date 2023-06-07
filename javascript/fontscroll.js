4855ecc247c52df2c73ad3e1e9092b9cc644000b
EhlonMusk <ehlonmusk@hotmail.com>
2023-06-07 12:58:34 +1000

    font color change

--- javascript/fontscroll.js
+++ javascript/fontscroll.js
@@ -1,16 +1,18 @@
 const container = document.querySelector(".container");
 const section = document.querySelector("section");
 container.addEventListener("scroll", function (event) {
-  console.log(section.classList);
+  console.log(container.scrollTop);
 });
 
 container.addEventListener("scroll", () => {
   const header = document.querySelector("header");
 
-  if (container.scrollTop > 0 && container.scrollTop < 5399) {
+  if (container.scrollTop > 300) {
     header.classList.add("scrolled");
-  } else {
+  } else if (container.scrollTop < 300) {
     header.classList.remove("scrolled");
+  } else {
+    header.classList.add("scrolled");
   }
 });
 
